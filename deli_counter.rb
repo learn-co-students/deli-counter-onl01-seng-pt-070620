331e006d17d950d4b1b34d92eba4bccc78f6ef5b
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-12 19:09:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def line(name)
+    place_in_line=[]
+    if name.length==0
+      puts "The line is currently empty."
+    else
+      name.each_with_index do |item,index|
+      place_in_line<< "#{index item}"
+     end
+   puts place_in_line 
+ end
+end
+
