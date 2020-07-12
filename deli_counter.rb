b21edabffb46c05ca03961aeffbb3290f94cdab9
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-12 19:31:42 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,11 @@ def line(name)
     if name.length==0
       puts "The line is currently empty."
     else
-      name.each_with_index do |item,index|
-      place_in_line<< "#{index item}"
+      name.each_with_index do |item, index|
+      place_in_line<< "#{index+1}. #{item}"
      end
-   puts place_in_line 
+   puts "The line is currently: #{place_in_line.join (" ")}"
  end
 end
+
 
