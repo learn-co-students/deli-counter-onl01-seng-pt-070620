22145ec4cfeffc24dbc16c1a8f6e329e4bcc3633
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-10-16 07:23:35 -0400

    solve again

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,26 +1,28 @@
-def line(array)
-  if array.any?
-    currenty_line = "The line is currently:"
-    array.each_with_index do |name,index|
-      currenty_line << " #{index+1}. #{name}"
-   end
-    
-   puts currenty_line
-  else
+def line(deli_array)
+
+  if deli_array.size == 0 # or if deli_array.any?
     puts "The line is currently empty."
-  end
-end
 
-def take_a_number(array,name)
-  array.push(name)
-  puts "Welcome, #{name}. You are number #{array.length} in line."
-end
-
-
-def now_serving(array)
-  if array.any?
-   puts "Currently serving #{array.shift}."
   else
+    # "The line is currently: 1. Logan 2. Avi 3. Spencer"
+    currenty_line = "The line is currently:"
+    deli_array.each_with_index do |name, index|
+      currenty_line << " #{index + 1}. #{name}"
+    end
+    puts currenty_line
+  end
+
+end
+
+def take_a_number(deli_array, name)
+  deli_array << name
+  puts "Welcome, #{name}. You are number #{deli_array.size} in line."
+end
+
+def now_serving(deli_array)
+  if deli_array.size == 0
     puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{deli_array.shift}."
   end
 end
